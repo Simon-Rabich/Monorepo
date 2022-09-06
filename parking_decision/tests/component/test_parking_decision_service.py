76c0fddaa7102d959b75ec2444a8fb72d196770e
Simon-Rabich <simonraviz1997@gmail.com>
2022-09-06 19:19:21 +0300

    added component tests

--- parking_decision/tests/component/test_parking_decision_service.py
+++ parking_decision/tests/component/test_parking_decision_service.py
@@ -1,25 +1,40 @@
 #!/usr/bin/env python3
+import json
+import multiprocessing
+from os import kill
+from signal import SIGKILL
 
-from typing import List, Optional
+import requests
+from unittest import TestCase
 
 from parking_decision.common.configurations.get_config import get_config
-from parking_decision.db.models.decision import Decision
+from parking_decision.db.migrations.setup_db_schema import setup_db_schema
 from parking_decision_service import ParkingDecisionService
 
 
-import requests
+class TestParkingDecisionService(TestCase):
 
+    service_process: multiprocessing.Process
 
-class TestParkingDecisionService(TestBase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.service_process: multiprocessing.Process = ParkingDecisionService.run()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        kill(cls.service_process.pid, SIGKILL)
+        cls.service_process.join()
 
     def setUp(self) -> None:
-        super().setUp()
+        setup_db_schema()
 
     def test_get_decision_parking_using_requests(self):
-        pass
+        # Arrange
+        payload = {'file': '/parking-decision-service/parking_decision/helpers/31d5L5y.jpeg',
+                   'name': "31d5L5y.jpeg"}
+        # Act
+        decision_result = requests.post(f'http://{get_config()["WEB_HOST"]}:{get_config()["WEB_PORT"]}/get_decision_parking',
+                                        data=payload)
 
-    def test_get_decision_parking_using_class(self):
-        pass
-
-    def test_pre_run(self):
-        pass
+        # Assert
+        self.assertTrue(json.loads(decision_result.content.decode()))
