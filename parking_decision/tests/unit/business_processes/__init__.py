2fe0f9a429cf71424e8348ef2fa41365cb7c029d
Simon-Rabich <simonraviz1997@gmail.com>
2022-09-12 19:24:13 +0300

    fixing init

--- parking_decision/tests/unit/business_processes/__init__.py
+++ parking_decision/tests/unit/business_processes/__init__.py
@@ -1,11 +1,4 @@
 #!/usr/bin/env python3
-#    ___                      _
-#   / __\   _ _ __ ___  _ __ | |_ ___  _ __ ___
-#  / / | | | | '_ ` _ \| '_ \| __/ _ \| '_ ` _ \
-# / /__| |_| | | | | | | |_) | || (_) | | | | | |
-# \____/\__, |_| |_| |_| .__/ \__\___/|_| |_| |_|
-#       |___/          |_|
-#
-# CYMPTOM LABS Copyright 2022. All rights reserved.
-#
-# Author: Naum Raviz (nraviz@tenable.com) on 21/07/2022
+
+# Author: Simon on 21/07/2022
+
